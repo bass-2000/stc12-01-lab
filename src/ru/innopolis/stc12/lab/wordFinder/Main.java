8e84fe751b8e7032ef9bac9f916e50f88c475a50
Alexander Boyko <bass-2000@yandex.ru>
2018-09-13 20:29:56 +0300

    added function that makes sources array from package of files

--- src/ru/innopolis/stc12/lab/wordFinder/Main.java
+++ src/ru/innopolis/stc12/lab/wordFinder/Main.java
@@ -1,14 +1,21 @@
 package ru.innopolis.stc12.lab.wordFinder;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class Main {
 
 
     public static void main(String[] args) throws EmptySourceException, IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
         String resultFile = "result";
-        String[] sourcesArray = {"C://TEMP//GENMEGENTLY//1.txt"};
+        String[] sourcesArray = getFIleNames("D://temp//testSet//");
         String[] words = {"starter", "smarter"};
 
 
@@ -23,4 +30,21 @@ public class Main {
         }
 
     }
+
+
+    public static String[] getFIleNames(String path) {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        List<String> results = new ArrayList<>();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                results.add(path + listOfFiles[i].getName());
+            }
+        }
+        return results.toArray(new String[0]);
+    }
 }
+
+
+
